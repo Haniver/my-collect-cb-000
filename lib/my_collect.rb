b07232172a4755adeaf69a985b1128adfa0618a0
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-04 18:23:11 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
 def my_collect(my_array)
   collection = []
   counter = 0
-  while counter < my_array.length
+  while counter < my_array.length do
     collection << yield my_array[counter]
     counter += 1
   end
